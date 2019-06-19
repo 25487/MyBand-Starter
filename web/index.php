facb62854a05705550af357f66a1fe54764e43c6
Taurese <43673192+25487@users.noreply.github.com>
2019-06-19 11:34:20 +0200

    Add files via upload
    de code, ik ben er nog mee bezig

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="autor" name="Taurese Usman MD1A">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <br>Yeet<br>
+</body>
+</html>
